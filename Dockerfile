7cdab5315fb7cd9b379fbb8cb08239515739e447
forest <forest.n.johnson@gmail.com>
2021-02-19 18:15:37 -0600

    add architechture agnostic build

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
-FROM ubuntu
 
-WORKDIR /picopublish
+FROM golang:1.15.2-alpine as build
+ARG GOARCH=
+ARG GO_BUILD_ARGS=
 
-ADD * /picopublish/
-ADD static/* /picopublish/static/
+RUN mkdir /build
+WORKDIR /build
+COPY . .
+RUN  go build -v $GO_BUILD_ARGS -o /build/picopublish .
 
-RUN chmod +x /picopublish/picopublish
-
-ENTRYPOINT /picopublish/picopublish
+FROM alpine
+WORKDIR /app
+COPY --from=build /build/static /app/static
+COPY --from=build /build/picopublish /app/picopublish
+RUN chmod +x /app/picopublish
+ENTRYPOINT ["/app/picopublish"]
---
+++ build-docker.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+VERSION="1.4.0"
+
+rm -rf dockerbuild || true
+mkdir dockerbuild
+
+cp Dockerfile dockerbuild/Dockerfile-amd64
+cp Dockerfile dockerbuild/Dockerfile-arm
+cp Dockerfile dockerbuild/Dockerfile-arm64
+
+sed -E 's|FROM alpine|FROM amd64/alpine|' -i dockerbuild/Dockerfile-amd64
+sed -E 's|FROM alpine|FROM arm32v7/alpine|'   -i dockerbuild/Dockerfile-arm
+sed -E 's|FROM alpine|FROM arm64v8/alpine|' -i dockerbuild/Dockerfile-arm64
+
+sed -E 's/GOARCH=/GOARCH=amd64/' -i dockerbuild/Dockerfile-amd64
+sed -E 's/GOARCH=/GOARCH=arm/'   -i dockerbuild/Dockerfile-arm
+sed -E 's/GOARCH=/GOARCH=arm64/' -i dockerbuild/Dockerfile-arm64
+
+docker build -f dockerbuild/Dockerfile-amd64 -t sequentialread/picopublish:$VERSION-amd64 .
+docker build -f dockerbuild/Dockerfile-arm   -t sequentialread/picopublish:$VERSION-arm .
+docker build -f dockerbuild/Dockerfile-arm64 -t sequentialread/picopublish:$VERSION-arm64 .
+
+docker push sequentialread/picopublish:$VERSION-amd64
+docker push sequentialread/picopublish:$VERSION-arm
+docker push sequentialread/picopublish:$VERSION-arm64
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+docker manifest create  sequentialread/picopublish:$VERSION \
+  sequentialread/picopublish:$VERSION-amd64 \
+  sequentialread/picopublish:$VERSION-arm \
+  sequentialread/picopublish:$VERSION-arm64 
+
+docker manifest annotate --arch amd64 sequentialread/picopublish:$VERSION sequentialread/picopublish:$VERSION-amd64
+docker manifest annotate --arch arm sequentialread/picopublish:$VERSION sequentialread/picopublish:$VERSION-arm
+docker manifest annotate --arch arm64 sequentialread/picopublish:$VERSION sequentialread/picopublish:$VERSION-arm64
+
+docker manifest push sequentialread/picopublish:$VERSION
+
+rm -rf dockerbuild || true
